94df467a380c2ec68c854835c231a56f2c48cee5
Sam Robinson <samwillrobinson@gmail.com>
2024-03-10 17:26:24 -0400

    added some styling

--- scripts.js
+++ scripts.js
@@ -1,35 +1,61 @@
-// Function to create a 3 x 3 grid
+// Create a Gameboard
+const gameboard = (function () {
+    const rows = 3;
+    const columns = 3;
+    let board = [];
 
-const board = (function createBoard() {
-    let rows = 3;
-    let cols = 3;
-    let grid = Array.from({ length: rows }, () => Array(cols).fill(null));
-
-    return grid;
-})();
-
-// Create Player Object Constructor
-let playerCounter = 0;
-
-function createPlayer(name, marker) {
-    playerCounter += 1;
-
-    if (playerCounter === 1) {
-        marker = 'X';
-    } else {
-        marker = 'O';
+    for (let i=0; i<rows; i++) {
+        board[i] = [];
+        for (let j=0; j<columns; j++) {
+            board[i][j] = null;
+        }
+    }
+    
+    function getBoard() {
+        return board;
     }
 
-    return {
-        id: playerCounter,
-        name: name,
-        marker: marker,
-        isPlayerOne: playerCounter === 1
-    };
+    function setCell(row, column, symbol) {
+        if (board[row][column] === null) {
+            board[row][column] = symbol;
+        } else {
+            console.log("that space is taken");
+        }
+        gameboard.checkBoard();
+        return gameboard.getBoard();
+    }
+
+    function checkBoard() {
+        for (let i=0; i<rows; i++) {
+            if (board[i][0] !== null && board[i][0]
+             === board[i][1] && board[i][1] === board[i][2]) {
+            console.log("You win!");}
+        }
+        for (let j=0; j<columns; j++) {
+            if (board[0][j] !== null && board[0][j]
+             === board[1][j] && board[1][j] === board[2][j]);
+        }
+        // Check diagonals
+        if (board[0][0] !== null && board[0][0]
+            === board[1][1] && board[1][1] === board[2][2]);
+    }
+
+    return { getBoard, setCell, checkBoard };
+})();
+
+Object.setPrototypeOf(gameboard, createPlayer);
+
+// Create Player factory function
+
+function createPlayer(player, marker) {
+    const name = player;
+    const symbol = marker;
+
+    return { name, symbol };
 }
 
-// Define Player 1 and Player 2
-const playerOne = createPlayer("Alice");
-const playerTwo = createPlayer("Bob");
+const game = (function() {
+    console.table(gameboard.getBoard())
+}());
 
 
